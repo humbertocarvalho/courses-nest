d4afe13559b845ede0397b0ab9707117cd5e86f7
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-06-09 21:06:27 -0300

    Hardcoded items list in service.

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { IItem } from './interfaces/item.interface';
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private readonly items: IItem[] = [
+    {
+      id: '123123123',
+      name: 'Item Onee',
+      qty: 150,
+      description: 'This is item one',
+    },
+    {
+      id: '123123123',
+      name: 'Item 2',
+      qty: 150,
+      description: 'This is item 2',
+    },
+  ];
+}
